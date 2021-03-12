591438c5e7f8a986fc9aa64f1764bb3d91d4d8b0
Pratik Sharma <prashar@microsoft.com>
2021-03-12 13:45:05 +0530

    push

--- simple-web-app-mvc-dotnet-master/SimpleWebAppMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleWebAppMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
